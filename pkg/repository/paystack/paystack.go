e0ca1ae0fcf7c13800d34dfecab4b1dd4072a367
lordscoba <lordscoba2tm@gmail.com>
2023-07-25 11:07:19 -0700

    test

--- pkg/repository/paystack/paystack.go
+++ pkg/repository/paystack/paystack.go
@@ -8,19 +8,22 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/joho/godotenv"
 	"github.com/lordscoba/bible_compass_backend/internal/config"
+	"github.com/lordscoba/bible_compass_backend/utility"
 )
 
 func PaystackInitPost(payload map[string]interface{}) (*resty.Response, error) {
-
-	// Set up the Paystack API endpoint
-	url := config.GetConfig().Paystack.InitUrl
-	// bearer := config.GetConfig().Paystack.PaystackKey
 
 	// Load the .env file into environment variables
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("Error loading .env file")
 		return nil, err
 	}
+
+	// Set up the Paystack API endpoint
+	url := config.GetConfig().Paystack.InitUrl
+	// bearer := config.GetConfig().Paystack.PaystackKey
+
+	logger := utility.NewLogger()
 	bearer := os.Getenv("PAYSTACK_KEY")
 
 	// Create a new Resty client
@@ -39,6 +42,8 @@ func PaystackInitPost(payload map[string]interface{}) (*resty.Response, error) {
 		fmt.Println(err)
 		return nil, err
 	}
+
+	logger.Info("success loading paystack info")
 
 	return response, nil
 }
